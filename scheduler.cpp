e5c1d749dbcf3ad2a8923abc7975e68e0c872c86
YimengL <liyimeng_csu@qq.com>
2014-10-10 10:51:56 -0400

    Update the FCFS methods

--- scheduler.cpp
+++ scheduler.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 pthread_mutex_t readyqueue_lock;
-pthread_cond_t condition_var1[9];
+pthread_cond_t condition_var1[20];
 pthread_cond_t con1;
 
 /* the element of ready_queue */
@@ -42,16 +42,14 @@ void init_scheduler(int sched_type) {
 	max_current_time = 0;		/* max among the several threads */
 	scheduler_type = sched_type;
 	/* initialize the mutex */
-	readyqueue_lock = PTHREAD_MUTEX_INITIALIZER;
-	con1 = PTHREAD_COND_INITIALIZER;
+	pthread_mutex_init(&readyqueue_lock, NULL);
 }
 
 int scheduleme(float currentTime, int tid, int remaining_time, int tprio) {
 	return_time = currentTime;
-	int retTime;
 	switch(scheduler_type) {
 		case FCFS:
-			retTime = fcfs_scheduleme(currentTime, tid, remaining_time, tprio);
+			return_time = fcfs_scheduleme(currentTime, tid, remaining_time, tprio);
 			break;
 		case SRTF:
 			break;
@@ -62,6 +60,7 @@ int scheduleme(float currentTime, int tid, int remaining_time, int tprio) {
 		default:
 			break;
 	}
+//	return_time = currentTime;
 	return return_time;
 }
 
@@ -78,7 +77,6 @@ int fcfs_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
 		th.priority = tprio;
 //		pthread_cond_init(&th.condition_var, NULL);
 		pthread_cond_init(&condition_var1[tid], NULL);
-//		con1 = PTHREAD_COND_INITIALIZER;
 		ready_queue.push_back(th);
 	}
 	
@@ -92,7 +90,6 @@ int fcfs_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
 	pthread_mutex_unlock(&readyqueue_lock);
 	
 	/* update the time */
-//	ready_queue.front().arrival_time = currentTime;
 	ready_queue.front().remaining_time = remainingTime;
 	
 	if (remainingTime <= 0) {
@@ -103,18 +100,14 @@ int fcfs_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
 //			pthread_cond_signal(&con1);
 			pthread_cond_signal(&condition_var1[ready_queue.front().id]);
 		}
-		
 		pthread_mutex_unlock(&readyqueue_lock);
 	}
+	
 	/* then, find the correct return time */
-	if (ceil(currentTime) > max_current_time)
-		max_current_time = ceil(currentTime);
-
-	if (max_current_time > return_time)
-		return_time = max_current_time;
+	if (ceil(currentTime) > return_time)
+		return_time = ceil(currentTime);
 	
 	return return_time;
-//	return max_current_time;
 }
 
 bool search_id(int id) {
@@ -133,29 +126,3 @@ ready_queue_t find_tid(int tid) {
 		}
 	}
 }
-	
-///* test the queue */
-//int main(int argc, char** argv) {
-	//ready_queue_t th;
-////	th = (ready_queue_t*)malloc(sizeof(ready_queue_t));
-	//th.id = 1;
-	//th.arrival_time = 2.0;
-	//th.remaining_time = 1;
-	//th.priority = 1;
-	//th.condition_var = PTHREAD_COND_INITIALIZER;
-	
-////	ready_queue_t th;
-	//th.id = 2;
-	//th.arrival_time = 2.2;
-	//th.remaining_time = 2;
-	//th.priority = 3;
-	//th.condition_var = PTHREAD_COND_INITIALIZER;
-	
-	//ready_queue.push_back(th);
-	
-	//cout << ready_queue.size() << endl;
-	//cout << search_id(1) << endl;
-	//cout << find_tid(2).arrival_time << endl;
-	
-	//return 0;
-//}
