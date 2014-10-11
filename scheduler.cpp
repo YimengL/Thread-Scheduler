9dd6030667748551f10fa953eef572b6c74d5ed6
YimengL <liyimeng_csu@qq.com>
2014-10-11 14:49:57 -0400

    fixed some useless part of the scheduler.c, but find a crucial bug when experimenting input_0, try to solve it now

--- scheduler.cpp
+++ scheduler.cpp
@@ -12,7 +12,9 @@
 using namespace std;
 
 pthread_mutex_t readyqueue_lock;
+pthread_mutex_t extra_lock;
 pthread_cond_t condition_var1[20];
+pthread_cond_t condition_var;
 pthread_cond_t con1;
 
 /* the element of ready_queue */
@@ -21,7 +23,6 @@ struct ready_queue_t {
 	float arrival_time;
 	int remaining_time;
 	int priority;
-//	pthread_cond_t condition_var;
 };
 
 list<ready_queue_t> ready_queue;
@@ -35,14 +36,28 @@ void init_scheduler(int sched_type);
 int sheduleme(float currentTime, int tid, int remainingTime, int tprio);
 int fcfs_scheduleme(float currentTime, int tid, int remainingTime, int tprio);		/* FCFS */
 int srtf_scheduleme(float currentTime, int tid, int remainingTime, int tprio);		/* SRTF */
+int pbs_scheduleme(float currentTime, int tid, int remainingTime, int tprio);		/* PBS */
+int mlfq_scheduleme(float currentTime, int tid, int remaining_time, int tprio);		/* MLFQ */
 bool search_id(int tid);															/* return if it is found */
-ready_queue_t find_tid(int tid);													/* return the struct be found */
 bool compare_remainingTime(const ready_queue_t& tmp1, const ready_queue_t& tmp2);	/* help to sort the list */
+bool compare_priority(const ready_queue_t& tmp1, const ready_queue_t& tmp2);		/* help to sort the list */
 
 /* compare function for the ready queue list */
-bool compare_remainingTime( const ready_queue_t& tmp1, const ready_queue_t& tmp2 )
-{
+bool compare_remainingTime( const ready_queue_t& tmp1, const ready_queue_t& tmp2 ) {
 	return tmp1.remaining_time < tmp2.remaining_time;
+}
+
+bool compare_priority(const ready_queue_t& tmp1, const ready_queue_t &tmp2) {
+	return (tmp1.priority < tmp2.priority);	
+}
+
+bool search_id(int id) {
+	for (list<ready_queue_t>::iterator it = ready_queue.begin(); it != ready_queue.end(); ++it) {
+		if (id == it->id) {
+			return true;
+		}
+	}		
+	return false;
 }
 
 void init_scheduler(int sched_type) {
@@ -51,11 +66,12 @@ void init_scheduler(int sched_type) {
 	scheduler_type = sched_type;
 	/* initialize the mutex */
 	pthread_mutex_init(&readyqueue_lock, NULL);
+	pthread_mutex_init(&extra_lock, NULL);
 }
 
 int scheduleme(float currentTime, int tid, int remaining_time, int tprio) {
-	return_time = currentTime;
 	switch(scheduler_type) {
+		pthread_mutex_lock(&extra_lock);
 		case FCFS:
 			return_time = fcfs_scheduleme(currentTime, tid, remaining_time, tprio);
 			break;
@@ -63,12 +79,15 @@ int scheduleme(float currentTime, int tid, int remaining_time, int tprio) {
 			return_time = srtf_scheduleme(currentTime, tid, remaining_time, tprio);
 			break;
 		case PBS:
+			return_time = pbs_scheduleme(currentTime, tid, remaining_time, tprio);
 			break;
 		case MLFQ:
+			return_time = mlfq_scheduleme(currentTime, tid, remaining_time, tprio);
 			break;
 		default:
 			break;
 	}
+	pthread_mutex_unlock(&extra_lock);
 	return return_time;
 }
 
@@ -76,7 +95,7 @@ int scheduleme(float currentTime, int tid, int remaining_time, int tprio) {
 int fcfs_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
 	
 	pthread_mutex_lock(&readyqueue_lock);
-
+	
 	/* case 1: if the input thread is not in the ready_queue, add it to the ready_queue */
 	if (!search_id(tid)) {
 		ready_queue_t th;
@@ -84,17 +103,13 @@ int fcfs_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
 		th.arrival_time = currentTime;
 		th.remaining_time = remainingTime;
 		th.priority = tprio;
-//		pthread_cond_init(&th.condition_var, NULL);
 		pthread_cond_init(&condition_var1[tid], NULL);
 		ready_queue.push_back(th);
 	}
 	
 	/* case 2: if the thread is not at the head of ready_queue, block it, wait until it go to the head */
 	if ( search_id(tid) && (ready_queue.front().id != tid)) {
-		ready_queue_t target;
-		target = find_tid(tid);
-		pthread_cond_wait(&condition_var1[target.id], &readyqueue_lock);
-//		pthread_cond_wait(&con1, &readyqueue_lock);
+		pthread_cond_wait(&condition_var1[tid], &readyqueue_lock);
 	}
 	pthread_mutex_unlock(&readyqueue_lock);
 	
@@ -106,14 +121,13 @@ int fcfs_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
 		ready_queue.pop_front();
 		
 		if (!ready_queue.empty()) {
-//			pthread_cond_signal(&con1);
 			pthread_cond_signal(&condition_var1[ready_queue.front().id]);
 		}
 		pthread_mutex_unlock(&readyqueue_lock);
 	}
 	
 	/* then, find the correct return time */
-	if (ceil(currentTime) > return_time)
+	if (ceil(currentTime) > (int)return_time)
 		return_time = ceil(currentTime);
 	
 	return return_time;
@@ -139,9 +153,7 @@ int srtf_scheduleme(float currentTime, int tid, int remainingTime, int tprio)
 	
 	/* case 2: if the thread is not at the head of ready_queue, block it, wait until it go to the head */
 	if ( search_id(tid) && (ready_queue.front().id != tid)) {
-		ready_queue_t target;
-		target = find_tid(tid);
-		pthread_cond_wait(&condition_var1[target.id], &readyqueue_lock);
+		pthread_cond_wait(&condition_var1[tid], &readyqueue_lock);
 	}
 	pthread_mutex_unlock(&readyqueue_lock);
 	
@@ -158,7 +170,6 @@ int srtf_scheduleme(float currentTime, int tid, int remainingTime, int tprio)
 		}
 		pthread_mutex_unlock(&readyqueue_lock);
 	}
-	
 	/* then, find the correct return time */
 	if (ceil(currentTime) > return_time)
 		return_time = ceil(currentTime);
@@ -166,19 +177,88 @@ int srtf_scheduleme(float currentTime, int tid, int remainingTime, int tprio)
 	return return_time;	
 }
 
-bool search_id(int id) {
-	for (list<ready_queue_t>::iterator it = ready_queue.begin(); it != ready_queue.end(); ++it) {
-		if (id == it->id) {
-			return true;
+/* Priority Based Scheduling */
+int pbs_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
+	
+	pthread_mutex_lock(&readyqueue_lock);
+
+	/* case 1: if the input thread is not in the ready_queue, add it to the ready_queue */
+	if (!search_id(tid)) {
+		ready_queue_t th;
+		th.id = tid;
+		th.arrival_time = currentTime;
+		th.remaining_time = remainingTime;
+		th.priority = tprio;
+		pthread_cond_init(&condition_var1[tid], NULL);
+		ready_queue.push_back(th);
+		ready_queue.sort(compare_priority);
+	}
+	
+	/* case 2: if the thread is not at the head of ready_queue, block it, wait until it go to the head */
+	if ( search_id(tid) && (ready_queue.front().id != tid)) {
+		pthread_cond_wait(&condition_var1[tid], &readyqueue_lock);
+	}
+	pthread_mutex_unlock(&readyqueue_lock);
+	
+	/* update the time */
+	ready_queue.front().remaining_time = remainingTime;
+	
+	if (remainingTime <= 0) {
+		pthread_mutex_lock(&readyqueue_lock);
+		ready_queue.pop_front();
+		
+		if (!ready_queue.empty()) {
+			ready_queue.sort(compare_priority);
+			pthread_cond_signal(&condition_var1[ready_queue.front().id]);
 		}
-	}		
-	return false;
+		pthread_mutex_unlock(&readyqueue_lock);
+	}
+	
+	/* then, find the correct return time */
+	if (ceil(currentTime) > return_time)
+		return_time = ceil(currentTime);
+	
+	return return_time;
 }
 
-ready_queue_t find_tid(int tid) {
-	for (list<ready_queue_t>::iterator it = ready_queue.begin(); it != ready_queue.end(); ++it) {
-		if (tid == it->id) {
-			return *it;
-		}
+int mlfq_scheduleme(float currentTime, int tid, int remainingTime, int tprio) {
+	
+	pthread_mutex_lock(&readyqueue_lock);
+	
+	if (!search_id(tid)) {
+//		cout << "Time: " << currentTime << ", Thread " << tid << " was insert!" << endl;
+		ready_queue_t th;
+		th.id = tid;
+		th.arrival_time = currentTime;
+		th.remaining_time = remainingTime;
+		th.priority = tprio;
+		pthread_cond_init(&condition_var, NULL);
+//		pthread_cond_init(&condition_var1[tid], NULL);
+		ready_queue.push_back(th);
 	}
+//	cout << "Time: " << currentTime << ", Thread " << tid << " was insert!" << endl;
+	pthread_mutex_unlock(&readyqueue_lock);
+	
+	if ( search_id(tid) && (ready_queue.front().id != tid)) {
+		pthread_cond_wait(&condition_var1[tid], &readyqueue_lock);
+//		pthread_cond_wait(&con1, &readyqueue_lock);
+	}
+//	pthread_mutex_unlock(&readyqueue_lock);
+	
+	/* update the time */
+	ready_queue.front().remaining_time = remainingTime;
+	
+	if (remainingTime <= 0) {
+//		pthread_mutex_lock(&readyqueue_lock);
+		ready_queue.pop_front();
+		
+		if (!ready_queue.empty()) {
+//			pthread_cond_signal(&con1);
+			pthread_cond_signal(&condition_var);
+		}
+//		pthread_mutex_unlock(&readyqueue_lock);
+	}
+	/*else*/ pthread_mutex_unlock(&readyqueue_lock);
+		
+	return return_time;
 }
